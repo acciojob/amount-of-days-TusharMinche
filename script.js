f63fbbdf85968c836921029f7ed055b695b44dc1
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-07-24 10:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+function isLeapYear(year){
+	if(year%4 == 0){
+		if(year%100 == 0){
+			if(year%400 == 0){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else {
+			return true;
+		}
+	}
+
+	else {
+		return false;
+	}
+}
+function daysOfAYear(year)  {
+	if(isLeapYear(year)){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
